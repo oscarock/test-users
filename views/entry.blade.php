f81bfb80edab1ae18759d73a6fe37742565b5342
Oscar Diaz <oscarock02@gmail.com>
2021-02-10 04:16:28 -0500

    fix validate sesion user

--- views/entry.blade.php
+++ views/entry.blade.php
@@ -7,36 +7,44 @@
 		<div class="row">
 	  		<div class="col-sm-12">
 				<br><br>
-			  <form class="row g-3" action="searchUsers" method="get" accept-charset="utf-8">
-					<div class="col-auto">
-						<label for="search" class="visually-hidden">Buscar</label>
-						<input type="text" class="form-control" name="search" id="search" placeholder="Ingrese nombre o email">
-					</div>
-					<div class="col-auto">
-						<button type="submit" class="btn btn-primary mb-3">Buscar</button>
-					</div>
-				</form>
-				<br>
-				<table class="table table-hover">
-					<tr>
-						<th>#</th>
-						<th>Documento</th>
-						<th>Nombre</th>
-						<th>Email</th>
-						<th>Pais</th>
-						<th>Fecha Creado</th>
-					</tr>
-					@foreach($users as $user)
+				@if(isset($authenticated))
+						<form class="row g-3" action="searchUsers" method="get" accept-charset="utf-8">
+						<div class="col-auto">
+							<label for="search" class="visually-hidden">Buscar</label>
+							<input type="text" class="form-control" name="search" id="search" placeholder="Ingrese nombre o email">
+						</div>
+						<div class="col-auto">
+							<button type="submit" class="btn btn-primary mb-3">Buscar</button>
+						</div>
+					</form>
+					<br>
+					<table class="table table-hover">
 						<tr>
-							<th>{{ $loop->iteration  }}</th>
-							<td>{{ $user->document }}</td>
-							<td>{{ $user->name }}</td>
-							<td>{{ $user->email }}</td>
-							<td>{{ $user->country }}</td>
-							<td>{{ $user->created_at }}</td>
+							<th>#</th>
+							<th>Documento</th>
+							<th>Nombre</th>
+							<th>Email</th>
+							<th>Pais</th>
+							<th>Fecha Creado</th>
 						</tr>
-					@endforeach
-				</table>
+						@foreach($users as $user)
+							<tr>
+								<th>{{ $loop->iteration  }}</th>
+								<td>{{ $user->document }}</td>
+								<td>{{ $user->name }}</td>
+								<td>{{ $user->email }}</td>
+								<td>{{ $user->country }}</td>
+								<td>{{ $user->created_at }}</td>
+							</tr>
+						@endforeach
+					</table>
+					@else
+					<div class="col-sm-12 mt-5">
+						<div class="alert alert-danger" role="alert">
+							<h3 class="text-center">No tienes permisos para ver este contenido<p><a href="/">Volver</a></p></h3>
+						</div>
+					</div>
+					@endif
 			</div>
 		</div>
 	</div>
